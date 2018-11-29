7e3329039e49cca7b3d7f8fc89294b317169ffc9
Ray Cao <youdymoo@umich.edu>
2018-11-28 20:13:04 -0500

    mongodb

--- mobility_dashboard/env/lib/python3.6/site-packages/mobility_dashboard/model.py
+++ mobility_dashboard/env/lib/python3.6/site-packages/mobility_dashboard/model.py
@@ -1,28 +1,23 @@
 import os
 import requests
 import mobility_dashboard
-from geojson import Feature
+from geojson import Feature, Point
 
 ROUTE = [
-    {"lat": 64.0027441, "long": -22.7066262},
-    {"lat": 64.0317168, "long": -22.1092311},
-    {"lat": 63.99879, "long": -21.18802},
-    {"lat": 63.4194089, "long": -19.0184548},
-    {"lat": 63.5302354, "long": -18.8904333},
-    {"lat": 64.2538507, "long": -15.2222918},
-    {"lat": 64.913435, "long": -14.01951},
-    {"lat": 65.2622588, "long": -14.0179538},
-    {"lat": 65.2640083, "long": -14.4037548},
-    {"lat": 66.0427545, "long": -17.3624953},
-    {"lat": 65.659786, "long": -20.723364},
-    {"lat": 65.3958953, "long": -20.9580216},
-    {"lat": 65.0722555, "long": -21.9704238},
-    {"lat": 65.0189519, "long": -22.8767959},
-    {"lat": 64.8929619, "long": -23.7260926},
-    {"lat": 64.785334, "long": -23.905765},
-    {"lat": 64.174537, "long": -21.6480148},
-    {"lat": 64.0792223, "long": -20.7535337},
-    {"lat": 64.14586, "long": -21.93955},
+    {"lat": 42.295353, "long": -83.717652, "name": "A1", "is_stop_location": True},
+    {"lat": 42.297133, "long": -83.749585, "name": "B1", "is_stop_location": False},
+    {"lat": 42.290750, "long": -83.769424, "name": "A2", "is_stop_location": True},
+    {"lat": 42.281176, "long": -83.778909, "name": "A3", "is_stop_location": True},
+    {"lat": 42.268800, "long": -83.759415, "name": "B2", "is_stop_location": False},
+    {"lat": 42.265116, "long": -83.749306, "name": "B3", "is_stop_location": False},
+    {"lat": 42.245311, "long": -83.748713, "name": "A4", "is_stop_location": True},
+    {"lat": 42.2622588, "long": -83.748112, "name": "A5", "is_stop_location": True},
+    {"lat": 42.242383, "long": -83.737498, "name": "A6", "is_stop_location": True},
+    {"lat": 42.253445, "long": -83.697748, "name": "B4", "is_stop_location": False},
+    {"lat": 42.270639, "long": -83.725687, "name": "B5", "is_stop_location": False},
+    {"lat": 42.277922, "long": -83.742615, "name": "A7", "is_stop_location": True},
+    {"lat": 42.283456, "long": -83.749112, "name": "A8", "is_stop_location": True},
+    {"lat": 42.291613, "long": -83.730212, "name": "A9", "is_stop_location": True},
 ]
 
 ROUTE_URL = "https://api.mapbox.com/directions/v5/mapbox/driving/{0}.json?access_token={1}&overview=full&geometries=geojson"
@@ -47,3 +42,21 @@ def get_route_data():
     route_data = Feature(geometry = geometry, properties = {})
 
     return route_data
+
+def create_stop_locations_details():
+    stop_locations = []
+    for location in ROUTE:
+        # Skip anything that is not a stop location
+        if not location["is_stop_location"]:
+            continue
+        # Create a geojson object for stop location
+        point = Point([location['long'], location['lat']])
+        properties = {
+            'title': location['name'],
+            'icon': 'campsite',
+            'marker-color': '#3bb2d0',
+            'marker-symbol': len(stop_locations) + 1
+        }
+        feature = Feature(geometry = point, properties = properties)
+        stop_locations.append(feature)
+    return stop_locations
--- mobility_dashboard/mobility_dashboard/views/demand.py
+++ mobility_dashboard/mobility_dashboard/views/demand.py
@@ -1,8 +1,39 @@
 import flask
 import mobility_dashboard
 from mobility_dashboard import model
-
+import json
+import os
 
 @mobility_dashboard.app.route('/demand/', methods=['GET'])
 def show_demand():
-    return flask.render_template("demand.html")
+    geo = read_demands()
+    # _demands()
+    return flask.render_template("demand.html", demand_data=geo)
+
+def read_demands():
+    geo = {}
+    with open('./mobility_dashboard/static/aa_demands.geojson', 'r') as geojson_file:
+        geo = json.load(geojson_file)
+    return geo
+
+def _demands():
+    geo = {}
+    with open('./mobility_dashboard/static/demands.geojson', 'r') as geojson_file:
+        geo = json.load(geojson_file)
+    new_geo = {"type": "FeatureCollection", "crs": { "type": "name", "properties": { "name": "Ann Arbor Demands" } }, "features": []}
+    with open('./mobility_dashboard/static/centroids.csv', 'r') as csv_file:
+        next(csv_file)
+        i = 0
+        for line in csv_file:
+            line = line.strip()
+            geo["features"][i]['geometry']['coordinates'][0] = line.split(',')[1]
+            geo["features"][i]['geometry']['coordinates'][1] = line.split(',')[2]
+            geo["features"][i]['properties']['id'] = line.split(',')[0]
+            # del geo["features"][i]['properties']['tsunami']
+            # del geo["features"][i]['properties']['felt']
+            new_geo["features"].append(geo["features"][i])
+            i += 1
+    with open('./mobility_dashboard/static/aa_demands.geojson', 'w+') as new_geojson_file:
+        r = json.dumps(new_geo)
+        new_geojson_file.write(str(r))
+    return new_geo
